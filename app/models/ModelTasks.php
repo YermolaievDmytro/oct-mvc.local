d2e08df05a4d4438ee85e941c25fbd92dc8b42c0
Vasyl <admin@gmail.com>
2019-04-25 20:25:27 +0300

    create model and change  core/models/Model.php

--- app/models/ModelTasks.php
+++ app/models/ModelTasks.php
@@ -7,23 +7,9 @@ use mysqli;
 
 class ModelTasks extends Model {
 
-    /**
-     *
-     * @var mysqli
-     */
-    protected $db;
-
     public function __construct() {
-        $this->db = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_NAME);
-    }
-
-    public function all() {
-        $query = "SELECT * FROM tasks;";
-        $result = $this->db->query($query);
-        if (!$result) {
-            return false;
-        }
-        return $result->fetch_all(MYSQLI_ASSOC);
+	parent::__construct();
+	$this->table = 'tasks';
     }
 
 //    public function add() {
@@ -31,21 +17,21 @@ class ModelTasks extends Model {
 //        //$query = "INSERT INTO tasks values (".$tasks_name.")";
 //        var_dump($tasks_name);
 //    }
-    
-    public function add(){
-        $task_name = filter_input(INPUT_POST, 'task_name');
-        $query = $this->db->escape_string("INSERT INTO tasks values(null, ".$task_name.");");
-        if(strlen($task_name)<1 || strlen($task_name)>254){
-            echo 'length of string is false';
-            return FALSE; //TODO echo error
-        }        
-        
-        $this->db->query($query);
-        if ($this->db->error) {
-            echo $this->db->error;
-            return FALSE;//TODO echo error
-        }
-        header('Location: /tasks ');
+
+    public function add() {
+	$task_name = filter_input(INPUT_POST, 'task_name');
+	$query = $this->db->escape_string("INSERT INTO tasks values(null, " . $task_name . ");");
+	if (strlen($task_name) < 1 || strlen($task_name) > 254) {
+	    echo 'length of string is false';
+	    return FALSE; //TODO echo error
+	}
+
+	$this->db->query($query);
+	if ($this->db->error) {
+	    echo $this->db->error;
+	    return FALSE; //TODO echo error
+	}
+	header('Location: /tasks ');
     }
 
 }
