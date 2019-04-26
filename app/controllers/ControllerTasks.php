24e35bfc2f82c44aa793cec87cdef3cc3fc65f49
Dmytro_Yermolaiev <iermolaiev1@gmail.com>
2019-04-27 00:55:57 +0300

    CRUD

--- app/controllers/ControllerTasks.php
+++ app/controllers/ControllerTasks.php
@@ -8,22 +8,42 @@ use models\ModelTasks;
 class ControllerTasks extends Controller {
 
     public function __construct() {
-	parent::__construct();
-	$this->model = new ModelTasks;
+        parent::__construct();
+        $this->model = new ModelTasks;
     }
 
     public function action_index() {
-	$this->view->tasks = $this->model->all();
-	$this->view->render('tasks_index_view');
+        $this->view->tasks = $this->model->all();
+        $this->view->render('tasks_index_view');
     }
-    
-    public function action_create(){
+
+    public function action_create() {
         $this->view->render('tasks_create_view');
     }
-    public function action_add(){
-	//TODO выбрать из поста, проверить и передать модели
+
+    public function action_add() {
+        //TODO выбрать из поста, проверить и передать модели
         $this->model->add();
-	// TODO редирект
+        // TODO редирект
+    }
+
+    public function action_delete() {
+        $id = $_POST['task_id'];
+        $this->model->delete($id);
+    }
+
+    public function action_updateF() {
+        $id = $_POST['task_id'];
+        $GLOBALS['task'] = $this->model->getbyid($id);
+        $this->view->render('tasks_update_view');
+//        $id = $_POST['task_id'];
+//        $this->model->update($id);
+    }
+
+    public function action_update() {
+        $id = $_POST['task_id'];
+        $name = $_POST['task_name'];
+        $this->model->update($id, $name);
     }
 
 }
--- app/views/tasks_index_view.php
+++ app/views/tasks_index_view.php
@@ -1,6 +1,6 @@
 <ul>
     <?php foreach ($this->tasks as $task): ?>
-    <li><?= $task['name'] ?></li>
-    <?php endforeach; ?>
+    <li><?= $task['name'] ?></li><form method="POST" action="/tasks/delete"> <input type="hidden" value="<?= $task['id']?>" name="task_id"/><input type="submit" value="delete"/><input type="submit" value="update" formaction="/tasks/updateF"/></form>
+    <?php endforeach;?>
     <a href="/tasks/create">Добавить задачу</a>
 </ul>
