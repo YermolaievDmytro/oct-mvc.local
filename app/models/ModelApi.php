ea86ffaf27f35a7f8d5269c2b76b50f759b77932
Vasyl <admin@gmail.com>
2019-04-27 12:23:06 +0300

    questions proccessing

--- app/models/ModelApi.php
+++ app/models/ModelApi.php
@@ -10,5 +10,15 @@ class ModelApi extends Model {
 	parent::__construct();
 	$this->table = 'questions';
     }
-
+    
+    public function add($question){
+	$query = 'insert into '.$this->table.' values (null,"'.$question["author"].'","'.$question["text"].'")';
+	$this->db->query($query);
+    }
+    
+    public function deletequestion($id){
+	//var_dump($id);
+	$query = 'DELETE FROM `oct_mvc`.'.$this->table.' WHERE `questions`.`id` = '.$id.';';
+	$this->db->query($query);
+    }
 }
--- js/questions.js
+++ js/questions.js
@@ -17,7 +17,7 @@
 //    xhr.send();
 //};
 
-function showQuestions () {
+function showQuestions() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '/api/questions');
     xhr.onreadystatechange = function () {
@@ -26,21 +26,73 @@ function showQuestions () {
 		var json_text = xhr.responseText;
 		var questions = JSON.parse(json_text);
 		var tbody = document.querySelector('#questions tbody');
-		console.log(questions);
 		tbody.innerHTML = '';
 		for (var i = 0; i < questions.length; i++) {
 		    var tr = '<tr>\n\
-<td>' + i + 1 + '</td>\n\
+<td>' + (i + 1) + '</td>\n\
 <td>' + questions[i].author + '</td>\n\
 <td>' + questions[i].text + '</td>\n\
+<td><form method="POST" class="deleteTask"><input type="hidden" name="delId" value="' + questions[i].id + '"><input type="submit" value="delete"></form></td>\n\
 </tr>';
 		    tbody.innerHTML += tr;
 		}
+		formDeleteFunc();
 	    } else {
 		return false;
 	    }
 	}
     };
     xhr.send();
-};
+}
 showQuestions();
+
+function sendQuestion(author, text) {
+    var post_data = 'author=' + author + '&text=' + text;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/addquestion');
+    xhr.onreadystatechange = function () {
+	if (xhr.readyState === 4) {
+	    if (xhr.status === 200) {
+		showQuestions();
+	    }
+	}
+    };
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(post_data);
+}
+
+function deleteQuestion(id) {
+    var post_data = 'id=' + id;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/deletequestion');
+    xhr.onreadystatechange = function () {
+	if (xhr.readyState === 4) {
+	    if (xhr.status === 200) {
+		showQuestions();
+	    }
+	}
+    };
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(post_data);
+}
+
+
+document.forms.question.onsubmit = function (event) {
+    var form_elements = this.elements;
+    var author = form_elements.author.value;
+    var text = form_elements.text.value;
+    this.reset();
+    sendQuestion(author, text);
+    event.preventDefault();
+};
+function formDeleteFunc() {
+    var forms_delete = document.querySelectorAll('.deleteTask');
+    for (var i = 0; i < forms_delete.length; i++) {
+	forms_delete[i].onsubmit = function (event) {	
+	    var id = this.elements.delId.value;
+	    deleteQuestion(id);
+	    event.preventDefault();
+	};
+    }
+}
+
